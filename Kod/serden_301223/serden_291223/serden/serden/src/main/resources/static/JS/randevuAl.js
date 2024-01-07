a9d9e13a8858bd23dd16087317b7ab03a0cb447c
Abdülkadir <123977817+abdulkadirsyk@users.noreply.github.com>
2024-01-07 13:27:17 +0300

    ..

--- Kod/serden_301223/serden_291223/serden/serden/src/main/resources/static/JS/randevuAl.js
+++
@@ -1,9 +0,0 @@
-function aileHekimiRandevu() {  
-    var abc = document.getElementById('clinicListDiv');
-    abc.style.display = 'none';
-}
-
-function hastaneRandevu() {
-    var abc = document.getElementById('clinicListDiv');
-    abc.style.display = 'block';
-}
--- hastane_drks1/hastane_drks1/hastane_drks/src/main/resources/static/JS/raporSorgula.js
+++
@@ -1,6 +0,0 @@
-function info(infoText) {
-    const infoElement = document.getElementById("info");
-    const textElement = document.getElementById("text");
-    infoElement.style.display = "block";
-    textElement.textContent = infoText;
-}
